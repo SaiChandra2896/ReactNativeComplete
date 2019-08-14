85bb6611ac7563ed174b164e39eeb7eca3f165c1
SaiChandra2896 <saichandra2896@gmail.com>
2019-08-14 16:34:33 +0530

    add reducer cases to get new state

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,3 +1,10 @@
+import {
+  ADD_PLACE,
+  DELETE_PLACE,
+  SELECT_PLACE,
+  DESELECT_PLACE
+} from '../actions/actionTypes';
+
 const initialstate = {
   places: [],
   selectedPlace: null
@@ -5,6 +12,38 @@ const initialstate = {
 
 const reducer = (state = initialstate, action) => {
   switch (action.type) {
+    case ADD_PLACE:
+      return {
+        ...state,
+        places: state.places.concat({
+          key: Math.random(),
+          name: action.placeName,
+          image: {
+            uri:
+              'http://trends.greatandhra.com/wp-content/uploads/2017/11/tankbund1.jpg'
+          }
+        })
+      };
+    case DELETE_PLACE:
+      return {
+        ...state,
+        places: state.places.filter(place => {
+          return place.key !== state.selectedPlace.key;
+        }),
+        selectedPlace: null
+      };
+    case SELECT_PLACE:
+      return {
+        ...state,
+        selectedPlace: state.places.find(place => {
+          return place.key === action.placeKey;
+        })
+      };
+    case DELETE_PLACE:
+      return {
+        ...state,
+        selectedPlace: null
+      };
     default:
       return state;
   }
